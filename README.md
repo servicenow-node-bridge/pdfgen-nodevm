f3ee1c07eb6cced7174af406db148bafa325d113
Richard Szolár <r@richardszolar.me>
2020-02-02 15:32:46 +0100

    - Initial commit of files and config

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # pdfgen-nodevm
 A pre-configured repo for generating PDF through the Bridge for Servicenow.
+
+# Setup
+1. Load the [Scoped app](https://github.com/servicenow-node-bridge/scoped-app) to your instance.
+2. Clone this repository
+```shell script
+$ git clone https://github.com/servicenow-node-bridge/pdfgen-nodevm.git
+```
+3. This repo can be used to generate a PDF with chrome+puppeteer through a Servicenow instance.
+
+If you have any suggestions or problems, don't hesitate to post an issue.
---
+++ servicenow-node-bridge/index.js
@@ -0,0 +1,86 @@
+var restify = require('restify');
+
+function processNativeScript(req, res, next) {
+    let status = "success";
+    let result = runScriptInVM(req.body.script, true);
+    if (result.error === true) {
+        status = "error";
+    }
+    let myObj = {
+        message: status,
+        result: result.result
+    };
+
+    res.send(myObj);
+    next();
+}
+
+function processNodeScript(req, res, next) {
+    let status = "success";
+    let result = runScriptInVM(req.body.script, false, req.body.log_sys_id, req.body.data);
+    if (result.error === true) {
+        status = "error";
+    }
+    let myObj = {
+        message: "success",
+        result: result.result
+    };
+
+    res.send(myObj);
+    next();
+}
+
+
+
+var server = restify.createServer();
+server.use(restify.plugins.bodyParser());
+server.post('/process/native', processNativeScript);
+server.post('/process/node', processNodeScript);
+
+server.listen(8080, function() {
+    console.log('%s listening at %s', server.name, server.url);
+});
+
+
+function runScriptInVM(scriptPayload, native, logID, data) {
+    let vm = null;
+    const {
+        VM,
+        NodeVM,
+        VMScript
+    } = require('vm2');
+    if (native === true) {
+        vm = new VM();
+    } else {
+        const _getLogID = () => logID;
+        const _getData = () => data;
+        let options = {
+            sandbox: {
+                _getLogID,
+                _getData
+            },
+            require: {
+                external: true,
+                builtin: ['fs', 'path', 'util'],
+                root: './',
+            }
+        };
+        vm = new NodeVM(options);
+    }
+
+    const script = scriptPayload;
+    let result;
+    let error = false;
+
+    try {
+        result = vm.run(script, 'vm.js');
+    } catch (e) {
+        error = true;
+        result = {message: e.toString(), stack: e.stack};
+    }
+
+    return {
+        error: error,
+        result: result
+    };
+}
